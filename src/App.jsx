e0b9656fe4cafebe564131fc1a68171fb932d9bf
antu468 <antudas468@gmail.com>
2025-12-11 22:12:24 +0600

    taking input from user

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Chai from './chai'
 function App() {
 
+  const username="Chai aur code"
 
   return (
+    <>
    <Chai/>
-    
+    <h1>chai aur react {username}</h1>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,22 @@ const anotherElement=(
   <a href="http://google.com" target='_blank'>Visit Google</a>
 )
 
+const anotheruser="chai aur react"
+
 const reactElement= React.createElement(
   //accepted first parameter is tag type
   'a',
   // second parameter is props object
   {href:'http://google.com', target:'_blank'},
   // third parameter is children
-  'Click me to visit Google'
+  'Click me to visit Google',
+  anotheruser
 )
 
 //console.log(reactElement)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
+
   reactElement
 
 )
